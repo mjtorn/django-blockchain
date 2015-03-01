09a772861111ee79269775abe52d83a56f2e0be1
Markus Törnqvist <mjt@fadconsulting.com>
2015-03-01 16:57:49 +0200

    Set up

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# vim: fileencoding=utf-8
+
+from distutils.core import setup
+
+setup(
+    name='django-blockchain',
+    version='0.0.1',
+    description='Implement something for Django to use blockchain.info APIs',
+    url='https://github.com/mjtorn/django-blockchain',
+    author=u'Markus Törnqvist',
+    author_email='markus.tornqvist@fadconsulting.com',
+)
+
